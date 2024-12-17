1e4e5eb9fabde9bcb1eefdfdf2816532844da40d
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2024-12-17 20:13:14 +0530

    first commit from initial creation

---
+++ src/test/java/demo/RestAssured.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class RestAssured
+{
+	public void test1()
+	{
+		System.out.println("Rest Assured Java");
+		System.out.println("Rest Assured C#");
+	}
+	
+	public void test2()
+	{
+		
+	}
+}
---
+++ src/test/java/demo/Selenium.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Selenium
+{
+	public void test1()
+	{
+		System.out.println("Selenium Java");
+		System.out.println("Selenium C#");
+	}
+}
