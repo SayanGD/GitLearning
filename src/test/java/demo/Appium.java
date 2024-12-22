b5bc9429a3a801fb5669be2ed3958bc66694c4d1
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2024-12-22 14:39:48 +0530

    undone

--- src/test/java/demo/Appium.java
+++ src/test/java/demo/Appium.java
@@ -4,11 +4,6 @@ public class Appium
 {
 	public void test1()
 	{
-		System.out.println("Appium Java");
-	}
-
-	public void test2()
-	{
-		System.out.println("Appium C#");
+		System.out.println("Appium Java ++");
 	}
 }
